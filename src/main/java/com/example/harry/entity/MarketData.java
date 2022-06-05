bbba0f3e192613451bd0246a66b25e646ccc39ef
hohangyau <hohangyau@gmail.com>
2022-06-06 02:08:40 +0800

    Update MarketData.java

--- src/main/java/com/example/harry/entity/MarketData.java
+++ src/main/java/com/example/harry/entity/MarketData.java
@@ -2,13 +2,17 @@ package com.example.harry.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class MarketData {
-    private BigDecimal price;
-    private LocalDateTime updateTime;
+    @Getter @Setter private String symbol;
+    @Getter @Setter private BigDecimal bid;
+    @Getter @Setter private BigDecimal ask;
+    @Getter @Setter private BigDecimal last;
+    @Getter @Setter private LocalDateTime updateTime;
+
+    @Override public String toString() {
+        return String.format("%s (bid: %s,ask: %s, last: %s, updateTime %s )", symbol, bid, ask, last, updateTime);
+      }
 }
